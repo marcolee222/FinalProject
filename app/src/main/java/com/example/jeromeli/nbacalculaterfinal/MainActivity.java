524054411fe876b6882a78baf225b06fe062717e
jeromeli <jialeli2@illinois.edu>
2018-12-11 01:55:20 -0600

    rua

--- app/src/main/java/com/example/jeromeli/nbacalculaterfinal/MainActivity.java
+++ app/src/main/java/com/example/jeromeli/nbacalculaterfinal/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button ESPNBtn = (Button) findViewById(R.id.ESPNBtn);
+        Button ESPNBtn = (Button)  findViewById(R.id.ESPNBtn);
         ESPNBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
